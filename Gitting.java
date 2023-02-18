93f5ce770242bf00d55c7a31968d1c24c3f881f7
faraaz817 <faraaz817@gmail.com>
2023-02-18 20:22:05 +0530

    Updated the branch 817

--- Gitting.java
+++ Gitting.java
@@ -9,5 +9,6 @@ class Gitting
 	public static void new_git_method() {
 		System.out.println("hello 2.0");
 		System.out.println("hello 3.0");
+		System.out.println("this change is for main only");
 	}
 }
